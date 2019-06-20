8c891e90942a6585f6a0987de2bfd0ccb03ecd72
Mark Abraham <markabraham104@gmail.com>
2019-06-20 10:52:10 -0600

    //Get /user, //Post /user, //Get /greeting

---
+++ data.js
@@ -0,0 +1,8 @@
+var data = {
+    user: {
+        name: "",
+        age: 0,
+    },
+};
+
+module.exports = data;
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require( "express");
+const cors = require( "cors" );
+
+var server = express( );
+var port = 8080;
+
+//Data
+var data = require( "./data.js");
+
+//Middleware
+server.use( cors( ) );
+server.use( express.json( ));
+server.use( express.urlencoded( {
+    extended: false
+} ) );
+
+//REST endpoints
+//Get /user
+server.get( "/user", function(req, res) {
+    if ( !data.user.name ) {
+        res.status( 400 );
+        var response = {
+            msg: "No name has been entered yet"
+        };
+        res.json( response );
+    } else {
+        var response = {
+            user: data.user.name,
+            age: data.user.age,
+            created_on: new Date ( ),
+        }
+        res.json( response );
+    }
+});
+
+//Post /user
+server.post( "/user", function(req, res) {
+    if ( !req.body.user.name ) {
+        res.status( 400 );
+        var response = {
+            msg: "Please enter a user name"
+        };
+        res.json(response);
+    } else if ( isNaN( req.body.user.age )) {
+        res.status( 400 );
+        var response = {
+            msg: "Please enter a valid age"
+        };
+        res.json( response );
+    } else {
+        var new_user = {
+            "name": req.body.user.name,
+            "age": req.body.user.age,
+            "created on": new Date ( )
+        }
+        data.user = new_user;
+        res.status( 201 );
+        res.send( );
+    }
+});
+
+//Get /greeting
+    server.get( "/greeting", function(req, res) {
+        if ( !data.user.name ) {
+            res.status( 400 );
+            var response = {
+                greeting: "Welcome"
+            };
+            res.json( response );
+        } else {
+            res.status( 201 );
+            var response = {
+                greeting: "Welcome " + data.user.name
+            }
+            res.json( response );
+        }
+    })
+
+server.listen( port, function ( ) {
+    console.log(`Listening on port ${port}`);
+});
